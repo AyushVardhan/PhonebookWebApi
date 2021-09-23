1ebd51c8ce4b41689c53501c65898113a68897c5
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2021-09-23 13:32:28 +0530

    Add unit test framework

---
+++ WebApiTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using Moq;
+
+namespace WebApiTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
